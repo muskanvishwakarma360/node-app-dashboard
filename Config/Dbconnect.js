71951f789c016bb73447007d12abe68502cfab48
muskanvishwakarma360 <muskan.vishwakarma@adglobal360.com>
2025-07-01 23:50:34 +0530

    my node app updated

--- Config/Dbconnect.js
+++ Config/Dbconnect.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-const connection = () => {
-    try {
-        mongoose.connect(process.env.MONGODB_URL)
-    } catch (err) {
-        console.error("connection err", err)
-    }
-}
+const connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true 
+    });
+
+    console.log("mongoDB connected successfully");
+  } catch (err) {
+    console.error("mongoDB connection error:", err);
+    process.exit(1); 
+  }
+};
 
 module.exports = connection;
